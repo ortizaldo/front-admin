4d688902b057a8b1600760e6ec12d505cc0cba38
Aldo Ortiz <ortizaldo14@gmail.com>
2023-10-27 17:06:57 -0600

    fix navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -30,16 +30,16 @@ export class NavbarComponent implements OnInit, OnDestroy {
     this.sidebarVisible = false;
   }
   // function that adds color white/transparent to the navbar on resize (this is for the collapse)
-   updateColor = () => {
-   var navbar = document.getElementsByClassName('navbar')[0];
-     if (window.innerWidth < 993 && !this.isCollapsed) {
-       navbar.classList.add('bg-white');
-       navbar.classList.remove('navbar-transparent');
-     } else {
-       navbar.classList.remove('bg-white');
-       navbar.classList.add('navbar-transparent');
-     }
-   };
+  updateColor = () => {
+    var navbar = document.getElementsByClassName('navbar')[0];
+    if (window.innerWidth < 993 && !this.isCollapsed) {
+      navbar.classList.add('bg-white');
+      navbar.classList.remove('navbar-transparent');
+    } else {
+      navbar.classList.remove('bg-white');
+      navbar.classList.add('navbar-transparent');
+    }
+  };
   ngOnInit() {
     window.addEventListener("resize", this.updateColor);
     this.listTitles = ROUTES.filter(listTitle => listTitle);
@@ -69,15 +69,13 @@ export class NavbarComponent implements OnInit, OnDestroy {
 
   sidebarOpen() {
     const toggleButton = this.toggleButton;
-    const mainPanel = <HTMLElement>(
-      document.getElementsByClassName("main-panel")[0]
-    );
+    const mainPanel: HTMLElement = document.getElementsByClassName("main-panel")[0] as HTMLElement;
     const html = document.getElementsByTagName("html")[0];
     if (window.innerWidth < 991) {
       mainPanel.style.position = "fixed";
     }
 
-    setTimeout(function() {
+    setTimeout(function () {
       toggleButton.classList.add("toggled");
     }, 500);
 
@@ -88,12 +86,11 @@ export class NavbarComponent implements OnInit, OnDestroy {
   sidebarClose() {
     const html = document.getElementsByTagName("html")[0];
     this.toggleButton.classList.remove("toggled");
-    const mainPanel = <HTMLElement>(
-      document.getElementsByClassName("main-panel")[0]
-    );
+
+    const mainPanel: HTMLElement = document.getElementsByClassName("main-panel")[0] as HTMLElement;
 
     if (window.innerWidth < 991) {
-      setTimeout(function() {
+      setTimeout(function () {
         mainPanel.style.position = "";
       }, 500);
     }
@@ -118,13 +115,13 @@ export class NavbarComponent implements OnInit, OnDestroy {
       if ($layer) {
         $layer.remove();
       }
-      setTimeout(function() {
+      setTimeout(function () {
         $toggle.classList.remove("toggled");
       }, 400);
 
       this.mobile_menu_visible = 0;
     } else {
-      setTimeout(function() {
+      setTimeout(function () {
         $toggle.classList.add("toggled");
       }, 430);
 
@@ -139,16 +136,16 @@ export class NavbarComponent implements OnInit, OnDestroy {
           .appendChild($layer);
       }
 
-      setTimeout(function() {
+      setTimeout(function () {
         $layer.classList.add("visible");
       }, 100);
 
-      $layer.onclick = function() {
+      $layer.onclick = function () {
         //asign a function
         html.classList.remove("nav-open");
         this.mobile_menu_visible = 0;
         $layer.classList.remove("visible");
-        setTimeout(function() {
+        setTimeout(function () {
           $layer.remove();
           $toggle.classList.remove("toggled");
         }, 400);
@@ -174,7 +171,7 @@ export class NavbarComponent implements OnInit, OnDestroy {
   }
 
   open(content) {
-    this.modalService.open(content, {windowClass: 'modal-search'}).result.then((result) => {
+    this.modalService.open(content, { windowClass: 'modal-search' }).result.then((result) => {
       this.closeResult = `Closed with: ${result}`;
     }, (reason) => {
       this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
@@ -187,10 +184,11 @@ export class NavbarComponent implements OnInit, OnDestroy {
     } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
       return 'by clicking on a backdrop';
     } else {
-      return  `with: ${reason}`;
+      return `with: ${reason}`;
     }
   }
-  ngOnDestroy(){
-     window.removeEventListener("resize", this.updateColor);
+
+  ngOnDestroy() {
+    window.removeEventListener("resize", this.updateColor);
   }
 }
--- src/app/layouts/auth-layout/auth-layout.routing.ts
+++ src/app/layouts/auth-layout/auth-layout.routing.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 
 import { RtlComponent } from '../../pages/rtl/rtl.component';
+import { LoginComponent } from 'src/app/pages/login/login.component';
 
 
 export const AuthLayoutRoutes: Routes = [
-    // { path: 'rtl',          component: RtlComponent },
+    { path: 'login', component: LoginComponent },
 ];
