1a5d59b12d511ca971c632a9183f01aedb7986f4
Aldo Ortiz <ortizaldo14@gmail.com>
2023-10-27 17:59:50 -0600

    auth component

--- src/app/layouts/auth-layout/auth-layout.routing.ts
+++ src/app/layouts/auth-layout/auth-layout.routing.ts
@@ -1,8 +1,17 @@
 import { Routes } from '@angular/router';
 
 import { LoginComponent } from 'src/app/pages/auth/login/login.component';
+import { RegisterComponent } from 'src/app/pages/auth/register/register.component';
+import { AuthLayoutComponent } from './auth-layout.component';
 
 
 export const AuthLayoutRoutes: Routes = [
-    { path: 'login', component: LoginComponent },
+  {
+    path: "auth",
+    children: [
+      { path: 'login', component: LoginComponent },
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent },
+    ],
+  },
 ];
