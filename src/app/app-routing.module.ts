5d9122ecf8a1145e6bdd73e45a241308c842cf0d
Aldo Ortiz <ortizaldo14@gmail.com>
2023-10-31 11:20:47 -0600

    style login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,7 +19,7 @@ const routes: Routes = [
   },
   {
     path: "",
-    redirectTo: "dashboard",
+    redirectTo: "auth/login",
     pathMatch: "full",
     canActivate: [AuthGuard],
   },
--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -5,6 +5,7 @@ import { TokenStorageService } from 'src/app/_services/token-storage.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
+  styleUrls: ["./login.component.css"],
 })
 export class LoginComponent implements OnInit {
   form: any = {
