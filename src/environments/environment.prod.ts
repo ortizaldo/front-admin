c40c93b89dc5c2d3fda8f20807cc8afae15c1a47
Aldo Ortiz <ortizaldo14@gmail.com>
2024-06-26 11:46:54 -0600

    se configura el front para cuando se genera el dist

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,9 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   authentication: false,
-  api: 'http://localhost:3000/api',
+  api: 'http://35.160.131.163:3000/api',
 };
 
 /*
