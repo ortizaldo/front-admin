99df085c345f88dfea151e5a9a9f741f56380ef9
Aldo Ortiz <ortizaldo14@gmail.com>
2026-01-25 00:16:55 -0600

    refactor: update CatalogDashboard component to enhance catalog display and remove unused code

--- src/app/pages/admin/catalog-dashboard/catalog-dashboard.component.ts
+++ src/app/pages/admin/catalog-dashboard/catalog-dashboard.component.ts
@@ -1,25 +1,50 @@
 import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
+import { UntypedFormBuilder } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
+import { CrudService } from "src/app/_services/crud.service";
 import Chart from "chart.js";
+// import { ConfirmationService, MessageService } from "primeng/api/public_api";
+import { tap } from "rxjs/internal/operators/tap";
+import { catchError } from "rxjs";
 
 @Component({
   selector: "app-catalog-dashboard",
   templateUrl: "catalog-dashboard.component.html",
 })
 export class CatalogDashboardComponent implements OnInit {
-  public canvas: any;
-  public ctx;
-  public datasets: any;
-  public data: any;
-  public myChartData;
-  public clicked: boolean = true;
-  public clicked1: boolean = false;
-  public clicked2: boolean = false;
+  catalog: any;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private fb: UntypedFormBuilder,
+    private crudService: CrudService,
+    private router: Router,
+  ) {}
 
   ngOnInit() {
     const id = this.route.snapshot.paramMap.get("id");
     console.log("🚀 ~ CatalogDashboardComponent ~ ngOnInit ~ id:", id);
+    this.getCatalog(id);
+  }
+
+  getCatalog(id: string) {
+    const params = {
+      filters: {
+        deleted: false,
+      },
+    };
+    this.crudService
+      .getMany("catalogs", id, params)
+      .pipe(
+        tap((data: any) => {
+          console.log("🚀 ~ CatalogsComponent ~ getCatalogs ~ data:", data);
+          this.catalog = data.data;
+        }),
+        catchError((err) => {
+          // this.loading = false;
+          return err;
+        }),
+      )
+      .subscribe();
   }
 }
